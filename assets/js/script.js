45de3e50a1fb24de00f4babe47b96ebd20d661fa
csamsul2017 <csamsul2017@gmail.com>
2024-12-19 21:35:18 +0700

    bug header and hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const header = document.getElementById('header');
+
 function myFunction() {
   const x = document.querySelector('.answare');
   if (x.style.display === 'none') {
@@ -6,3 +8,13 @@ function myFunction() {
     x.style.display = 'none';
   }
 }
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    // Jika scroll lebih dari 50px
+    header.style.height = '60px';
+  } else {
+    // Jika kembali ke atas
+    header.style.height = '80px';
+  }
+});
